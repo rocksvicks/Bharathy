eeceb4b6ecec6e69fa8b813851d590c4c65bfb5f
Rocks Vicks YT <57277354+rocksvicksyt@users.noreply.github.com>
2019-12-29 07:02:05 +0530

    Welcome change

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -30,6 +30,7 @@ My கவிதை தளம் @EnnaSollaPogirai.
 This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/rocksvicksyt/Bharathy),
 you can create clone same like me..
 Find me a [Bharathy Bot](https://t.me/Bharathy_bot)
+My brother [MSRI Editz](https://t.me/MSRI_Editz_bot)
 
 For more commands click /help...
 
